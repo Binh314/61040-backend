50e2aff38ae94818e70f3da4048aacd9b2c31e2b
Binh314 <binhphamc2002@gmail.com>
2023-10-05 19:59:04 -0400

    removed console log exposing api key

--- server/concepts/location.ts
+++ server/concepts/location.ts
@@ -16,8 +16,6 @@ export default class LocationConcept {
     const myAPIKey = process.env.GOOGLE_API_KEY;
     const geocodingUrl: string = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${myAPIKey}`;
 
-    console.log(geocodingUrl);
-
     const response = await fetch(geocodingUrl);
     const results = await response.json();
     if (results.results.length === 0) {
