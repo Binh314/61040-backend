b2721754949a5822479335cae2fbc3106ae6ad47
Binh314 <binhphamc2002@gmail.com>
2023-10-05 21:37:05 -0400

    added distance function

--- server/concepts/location.ts
+++ server/concepts/location.ts
@@ -24,4 +24,19 @@ export default class LocationConcept {
     const location = results.results[0].geometry.location;
     return location;
   }
+
+  /**
+   * Source: https://henry-rossiter.medium.com/calculating-distance-between-geographic-coordinates-with-javascript-5f3097b61898
+   */
+  private cosineDistanceBetweenPoints(lat1: number, lon1: number, lat2: number, lon2: number) {
+    const R = 6371e3;
+    const p1 = (lat1 * Math.PI) / 180;
+    const p2 = (lat2 * Math.PI) / 180;
+    const deltaP = p2 - p1;
+    const deltaLon = lon2 - lon1;
+    const deltaLambda = (deltaLon * Math.PI) / 180;
+    const a = Math.sin(deltaP / 2) * Math.sin(deltaP / 2) + Math.cos(p1) * Math.cos(p2) * Math.sin(deltaLambda / 2) * Math.sin(deltaLambda / 2);
+    const d = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)) * R;
+    return d;
+  }
 }
